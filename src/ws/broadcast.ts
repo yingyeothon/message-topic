16e35770f7c9a62adbb2577f74446644d67e56b6
jaeyoung.choi <lactrious@gmail.com>
2019-06-21 14:14:39 +0900

    Add _me, _now, _topic into broadcast message

--- src/ws/broadcast.ts
+++ src/ws/broadcast.ts
@@ -7,18 +7,41 @@ import { replyApi } from '../utils/ws';
 export const broadcastMessage: APIGatewayProxyHandler = messageApi<{
   action: 'broadcast';
   topic: string;
-  payload: string;
+  payload: any;
 }>(async ({ user, message, event }) => {
   if (!message.topic || !message.payload) {
     return { statusCode: 500, body: 'No data' };
   }
+
+  const payload =
+    typeof message.payload === 'object'
+      ? message.payload
+      : { payload: message.payload };
+  const now = Date.now();
+  const payloadForMe = JSON.stringify({
+    ...payload,
+    _topic: message.topic,
+    _now: now,
+    _me: true,
+  });
+  const payloadForOthers = JSON.stringify({
+    ...payload,
+    _topic: message.topic,
+    _now: now,
+    _me: false,
+  });
+
   const reply = replyApi(event);
   const errorMembers: string[] = [];
   for (const member of await getTopicRepo().getTopicMembers(message.topic)) {
     try {
       const connectionId = await getUserRepo().getConnectionIdFromUser(member);
-      console.log(`Broadcast`, user, member, message.payload, connectionId);
-      const sent = await reply(message.payload, connectionId);
+      const me = event.requestContext.connectionId === connectionId;
+      const payloadToSend = me ? payloadForMe : payloadForOthers;
+
+      console.log(`Broadcast`, user, member, payloadToSend, connectionId);
+      const sent = await reply(payloadToSend, connectionId);
+
       console.log(`Broadcast`, `sent`, user, member, sent);
     } catch (error) {
       console.error(`Broadcast`, `error`, member, error);
